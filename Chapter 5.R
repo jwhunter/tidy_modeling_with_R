0e50695c66396d26141ed6c1e7f95b944e7bd191
Jacob Hunter <jhunter@aofund.org>
2024-01-11 17:11:10 -0600

    Chapter 6 and 7

--- Chapter 5.R
+++ Chapter 5.R
@@ -1,6 +1,8 @@
 # Chapter 5: Spending our Data ----
 ## 5.1 Common Methods for Splitting Data ----
 
+
+
 # When data are reused for multiple tasks, instead of carefully 'spent'
 # from the finite data budget, certain risks increase, such as the risk of
 # accentuating bias or compounding effects from methodological errors
@@ -10,3 +12,5 @@
 # Primary approach is normally training and test sets: 2 set
 # training is the sandbox where exploration, feature engineering, and
     # model fitting happens
+
+asdfkj 
